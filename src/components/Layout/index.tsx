30417e92e875d60c5d45565fe5cf7e0d51193d33
Zmeev06 <se.rg.ey06@icloud.com>
2023-07-09 06:28:55 +0300

    fix: header

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,18 +4,23 @@ import { Outlet } from "react-router-dom";
 import Container from "../UI/Container";
 import Footer from "../Footer";
 import HeaderSidebar from "../HeaderSidebar";
+import styles from "./index.module.scss";
 
 const Layout = () => {
   return (
     <>
       <Container>
         <Header />
-        <HeaderSidebar />
+        
       </Container>
-      {/* <Outlet />
-      <Container>
+      <HeaderSidebar />
+      <div className={styles.content}>
+        <Outlet />
+
+        {/* <Container>
         <Footer />
       </Container> */}
+      </div>
     </>
   );
 };

6b06d50c9763c7659057de89b168f1d64e294e91
Zmeev06 <se.rg.ey06@icloud.com>
2023-07-18 07:08:32 +0300

    f

--- src/services/offersServices.ts
+++ src/services/offersServices.ts
@@ -14,3 +14,23 @@ export const getAllSellerOrders = async (token: string) => {
     },
   });
 };
+
+export const createOffer = async(type: number, token: string, currency: number, limit: number, price: number, paymentMethod: number, limitStart: number, limitEnd: number, requisites: string) => {
+  return await ApiClient({
+    url: "offer/create",
+    method: "POST",
+    headers: {
+      Authorization: `${token}`,
+    },
+    data: {
+      type,
+      currency, 
+      limit,
+      price, 
+      payment_method: paymentMethod, 
+      limit_start: limitStart, 
+      limit_end: limitEnd, 
+      requisites
+    },
+  });
+};
